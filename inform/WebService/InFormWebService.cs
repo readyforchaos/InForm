6f718e87419e375f4dfe287085ed7b09785da8d4
kkho <khiem_89@hotmail.com>
2016-03-06 17:51:48 +0100

    added webservice config

---
+++ inform/WebService/InFormWebService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace inform.WebService
+{
+    public class InFormWebService
+    {
+        private string apiKey = WebConfigurationManager.AppSettings["7vUYNtX0bTtsrL8+bMqbJ9W0wGXGxfolBWidCR4LHDJQNdDKkzGHWaI+8iCT3h9cft9KBQPAbEm5cP91edlLbg=="];
+
+
+
+
+
+    }
+}
